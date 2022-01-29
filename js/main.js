6449e497b30bcd66bf05c95e2c6c34df3475c7ef
JClark <mlitvinkox96@gmail.com>
2022-01-29 10:50:35 -0500

    Minor CSS Changes

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function openLeftNav() {
+    document.getElementById("left-nav").style.height = "100vh";
+}
+
+function closeLeftNav() {
+    document.getElementById("left-nav").style.height = "0";
+}
